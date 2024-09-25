16878e6a3eb21da32207c5d1249ab2c5af80abb3
Gabriel Alves <164409557+Gadelha1@users.noreply.github.com>
2024-09-24 23:25:30 -0300

    Adicionei mais rápido por upload
    tudo que eu consegui fazer com a ajuda do lucas

---
+++ ProvaWEB/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cfcc2949b434c64c05e9e6076c1cb01381b0290f',
+        'name' => 'avaliacao/web',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'avaliacao/web' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cfcc2949b434c64c05e9e6076c1cb01381b0290f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
